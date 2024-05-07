33b0ab64878953387d35e057b8015a67ccc7fa37
Thuan Nguyen <thuanphuoc@gmail.com>
2024-05-07 22:49:09 +0700

    update

--- myubuntu.sh
+++ myubuntu.sh
@@ -68,8 +68,8 @@ echo "$(whoami) ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/sudo_without_
 
 ################### kubernetes ####################
 # kubectl
-sudo curl --output-dir /usr/local/bin -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo chmod a+x /usr/local/bin/kubectl
+sudo curl --output-dir /tmp -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && \
+sudo mv /tmp/kubectl /usr/local/bin && sudo chmod u+x /usr/local/bin/kubectl
 kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
 grep 'complete -o default -F __start_kubectl kube' $USER_HOME/.bashrc > /dev/null
 if [[ $? -ne 0 ]]
@@ -78,16 +78,18 @@ then
 fi
 
 # kubectx & kubens
-sudo curl --output-dir /usr/local/bin -o kubectx $GIT_CONTENT_URL/kubectx/kubectx.sh
-sudo curl --output-dir /usr/local/bin -o kubens $GIT_CONTENT_URL/kubectx/kubens.sh
-sudo chmod a+x /usr/local/bin/kubectx
-sudo chmod a+x /usr/local/bin/kubens
-sudo curl --output-dir /etc/bash_completion.d -O $GIT_CONTENT_URL/kubectx/completion/kubectx.bash
-sudo curl --output-dir /etc/bash_completion.d -O $GIT_CONTENT_URL/kubectx/completion/kubens.bash
+sudo curl --output-dir /tmp -LO $GIT_CONTENT_URL/kubectx/kubectx.sh && \
+  sudo mv /tmp/kubectx.sh /usr/local/bin/kubectx && sudo chmod u+x /usr/local/bin/kubectx
+sudo curl --output-dir /tmp -LO $GIT_CONTENT_URL/kubectx/kubens.sh && \
+  sudo mv /tmp/kubens.sh /usr/local/bin/kubens && sudo chmod u+x /usr/local/bin/kubens
+sudo curl --output-dir /tmp -LO $GIT_CONTENT_URL/kubectx/completion/kubectx.bash && \
+  sudo mv /tmp/kubectx.bash /etc/bash_completion.d
+sudo curl --output-dir /tmp -LO $GIT_CONTENT_URL/kubectx/completion/kubens.bash && \
+  sudo mv /tmp/kubens.bash /etc/bash_completion.d
 
 # kube-ps1
-sudo curl --output-dir /usr/local/bin -O $GIT_CONTENT_URL/kube-ps1/kube-ps1.sh
-sudo chmod a+x /usr/local/bin/kube-ps1.sh
+sudo curl --output-dir /tmp -LO $GIT_CONTENT_URL/kube-ps1/kube-ps1.sh && \
+  sudo mv /tmp/kube-ps1.sh /usr/local/bin && sudo chmod u+x /usr/local/bin/kube-ps1.sh
 grep 'source /usr/local/bin/kube-ps1.sh' $USER_HOME/.bashrc > /dev/null
 if [[ $? -ne 0 ]]
 then
