ca6a8cd0c001164ffd6e2119f50e011eaaffc816
Thuan Nguyen <thuanphuoc@gmail.com>
2024-05-07 17:48:24 +0700

    Fix color

--- myubuntu.sh
+++ myubuntu.sh
@@ -16,18 +16,15 @@ then
 fi
 
 ################### ssh ##########################
-
 # ssh-agent cahce SSH credentials passphrase
 grep 'eval $(ssh-agent) && ssh-add ~/.ssh/id_rsa' $USER_HOME/.bashrc
 if [[ $? -ne 0 ]]
 then
   echo 'eval $(ssh-agent) && ssh-add ~/.ssh/id_rsa' >> $USER_HOME/.bashrc
 fi
-
 ##################################################
 
 ################### aliases ######################
-
 # sudo 
 grep 'alias sudo=' $USER_HOME/.bashrc > /dev/null
 if [[ $? -ne 0 ]]
@@ -41,25 +38,30 @@ if [[ $? -ne 0 ]]
 then
   echo "alias kube='kubectl'" >> $USER_HOME/.bashrc
 fi
-
 ###################################################
 
 #################### vim ##########################
-
 # set vim as default editor
 sudo update-alternatives --set editor /usr/bin/vim.basic
 
 # set paste mode
-cat $USER_HOME/.vimrc | grep "set paste" > /dev/null
+grep 'set paste' $USER_HOME/.vimrc > /dev/null
 if [[ $? -ne 0 ]]
 then
   echo "set paste" >> $USER_HOME/.vimrc
 fi
+###################################################
 
+############### terminal color ####################
+# fix color of 777 directory
+grep "^OTHER_WRITABLE" $USER_HOME/.dircolors
+if [[ $? -ne 0 ]]
+then
+  echo 'OTHER_WRITABLE 34' >> ~/.dircolors
+fi
 ###################################################
 
 ################### kubernetes ####################
-
 # kubectl
 sudo curl --output-dir /usr/local/bin -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod a+x /usr/local/bin/kubectl
@@ -86,7 +88,6 @@ if [[ $? -ne 0 ]]
 then
   echo 'PS1="$(kube_ps1) \u:\[\e[0;33m\]\w\[\e[0m\]\$ "' >> $USER_HOME/.bashrc
 fi
-
 ###################################################
 
 echo "DONE!!!"
