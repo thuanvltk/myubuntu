2cf28e8e07a36fdf0a744f21fe166740cfb9f6b7
Thuan Nguyen <thuanphuoc@gmail.com>
2024-05-18 17:30:13 +0700

    update

--- myubuntu.sh
+++ myubuntu.sh
@@ -3,9 +3,15 @@
 [[ -n "$MYDEBUG" ]] && set -o xtrace
 
 # Install common packages
+sudo apt-get update &> /dev/null
 sudo apt-get install -y \
   unzip \
+  apt-transport-https \
+  ca-certificates \
+  gnupg \
+  lsb-release
   curl &> /dev/null
+sudo mkdir -p /etc/apt/keyrings
 
 # Declare variables
 GIT_CONTENT_URL='https://raw.githubusercontent.com/thuanvltk/myubuntu/main'
@@ -88,6 +94,27 @@ then
 else
   sudo /tmp/aws/install --update
 fi
+
+# azcli
+curl -sLS https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /etc/apt/keyrings/microsoft.gpg
+sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
+AZ_DIST=$(lsb_release -cs)
+echo "Types: deb
+URIs: https://packages.microsoft.com/repos/azure-cli/
+Suites: ${AZ_DIST}
+Components: main
+Architectures: $(dpkg --print-architecture)
+Signed-by: /etc/apt/keyrings/microsoft.gpg" | sudo tee /etc/apt/sources.list.d/azure-cli.sources
+sudo apt-get update &> /dev/null
+sudo apt-get install -y azure-cli &> /dev/null
+
+# az-ps1
+sudo curl --output-dir /tmp -LO "$GIT_CONTENT_URL/az-ps1/az-ps1.sh" && \
+  sudo mv /tmp/az-ps1.sh /usr/local/bin && sudo chmod a+x /usr/local/bin/az-ps1.sh
+if [[ ! $(grep 'source /usr/local/bin/az-ps1.sh' "$USER_HOME"/.bashrc &> /dev/null) ]]
+then
+  echo 'source /usr/local/bin/az-ps1.sh' >> "$USER_HOME"/.bashrc
+fi
 ###################################################
 
 ################### kubernetes ####################
@@ -124,11 +151,6 @@ if [[ $? -ne 0 ]]
 then
   echo 'source /usr/local/bin/kube-ps1.sh' >> "$USER_HOME"/.bashrc
 fi
-grep 'PS1="$(kube_ps1)' $USER_HOME/.bashrc &> /dev/null
-if [[ $? -ne 0 ]]
-then
-  echo 'PS1="$(kube_ps1) \u:\[\e[0;33m\]\w\[\e[0m\]\$ "' >> "$USER_HOME"/.bashrc
-fi
 
 # helm
 HELM_VERSION=$(curl -sL "https://get.helm.sh/helm-latest-version")
@@ -138,6 +160,13 @@ sudo curl --output-dir /tmp -LO "https://get.helm.sh/helm-${HELM_VERSION}-linux-
   sudo mv /tmp/helm/linux-amd64/helm /usr/local/bin/helm && \
   sudo chmod a+x /usr/local/bin/helm
 helm completion bash | sudo tee /etc/bash_completion.d/helm &> /dev/null
+###################################################
+
+################### set PS1 #######################
+if [[ ! $(grep 'PS1="$(az_ps1) $(kube_ps1)' "$USER_HOME"/.bashrc &> /dev/null) ]]
+then
+  echo 'PS1="$(az_ps1) $(kube_ps1) \u:\[\e[0;33m\]\w\[\e[0m\]\$ "' >> "$USER_HOME"/.bashrc
+fi
 ###################################################
 
 echo "DONE!!!"
