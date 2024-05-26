1cd2ad5cd75d342ee8abc71076688d8838e07144
Thuan Nguyen <thuanphuoc@gmail.com>
2024-05-26 16:11:16 +0700

    update

--- az-ps1/az-ps1.sh
+++ az-ps1/az-ps1.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
+# Color variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+# Clear the color after that
+CLEAR='\033[0m'
+
 az_ps1()
 {
-  az_subscription=$(az account show 2> /dev/null | jq -r '.name')
-  if [[ -n "$az_subscription" ]]
+  AZ_SUBSCRIPTION=$(az account show 2> /dev/null | jq -r '.name')
+  if [[ -n "$AZ_SUBSCRIPTION" ]]
   then
-    echo "(🔑$az_subscription)"
+    echo -e "(${YELLOW}🔑${CLEAR}$AZ_SUBSCRIPTION)"
   else
     echo "(🔑)"
   fi
