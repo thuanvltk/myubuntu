6c4ca02343c5baa488cec66842faa58e2ad2dd42
Thuan Nguyen <thuanphuoc@gmail.com>
2024-05-26 15:42:02 +0700

    update

--- az-ps1/az-ps1.sh
+++ az-ps1/az-ps1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 az_ps1()
 {
-  az_subscription=$(az account show 2> /dev/null | jq .name)
+  az_subscription=$(az account show 2> /dev/null | jq -r '.name')
   if [[ -n "$az_subscription" ]]
   then
     echo "(🔑$az_subscription)"
