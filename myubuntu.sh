af072aff848c413019c92a27817211b303963d70
Thuan Nguyen <thuanphuoc@gmail.com>
2024-05-31 16:30:10 +0700

    update

--- myubuntu.sh
+++ myubuntu.sh
@@ -125,9 +125,11 @@ sudo ln -sf "$USER_HOME"/.local/bin/az-switch /usr/local/bin/az-switch
 
 ################### terraform #####################
 # tfenv
-sudo curl --output-dir /tmp -LO "$GIT_CONTENT_URL/tfenv/tfenv.sh" && \
-  sudo mv /tmp/tfenv.sh /usr/local/bin/tfenv && \
-  sudo chmod a+x /usr/local/bin/tfenv
+sudo mkdir -p /tmp/tfenv && \
+  sudo curl --output-dir /tmp/tfenv -LO "$GIT_CONTENT_URL/tfenv/v3.0.zip" && \
+  sudo unzip -o /tmp/tfenv/v3.0.zip -d /tmp/tfenv &> /dev/null
+sudo cp -r /tmp/tfenv/v3.0 /usr/local/bin/tfenv && \
+  sudo ln -s /usr/local/bin/tfenv/* /usr/local/bin
 tfenv install latest
 ###################################################
 
